6a285eb8f4174a1ba0f13ac358ea54f700b46272
khoand1992 <143287942+khoand1992@users.noreply.github.com>
2023-09-11 02:52:12 +0700

    Add SendEmail Package - RealPython page

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from hellopackage.helloworld import helloboy
 from hellopackage.printStatus import print_status
 from readCSV.readData import readTitanicData
 from APIEndpoint.JsonPlaceHolder import getResponseJSON
+from SendEmail.RealPython import sendEmail
 
 
 if __name__ == "__main__":
@@ -11,3 +12,4 @@ if __name__ == "__main__":
     readTitanicData(data_path)
     df = getResponseJSON('https://jsonplaceholder.typicode.com/posts/1')
     print(df)
+    sendEmail()
