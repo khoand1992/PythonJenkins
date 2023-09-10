eb8cee17de12b36d8981a46136571b2eaded93e1
khoand1992 <143287942+khoand1992@users.noreply.github.com>
2023-09-10 07:13:15 +0700

    Add printStatus

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 from hellopackage.helloworld import helloboy
+from hellopackage.printStatus import print_status
 
 if __name__ == "__main__":
     helloboy()
+    print_status()
---
+++ hellopackage/printStatus.py
@@ -0,0 +1,2 @@
+def print_status():
+    print("Status fine")
